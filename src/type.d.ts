9381f8eeb98f58b780ab542af98e70790d6c2595
Joshua Avalon <git@avalon.sh>
2026-05-31 22:29:17 +0800

    chore: Fix lint

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,12 +1,13 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import type { Linter } from "eslint";
 
 declare module "eslint-plugin-react-hooks" {
   export const configs: {
-    recommended: Linter.Config,
-    'recommended-latest': Linter.Config,
     flat: {
-      recommended: Linter.Config,
-      'recommended-latest': Linter.Config
-    }
+      recommended: Linter.Config;
+      "recommended-latest": Linter.Config;
+    };
+    recommended: Linter.Config;
+    "recommended-latest": Linter.Config;
   };
 }
