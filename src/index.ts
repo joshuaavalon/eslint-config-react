a4642dfa97cea4ce3259648f8aeea53cf3c309a8
Joshua Avalon <git@avalon.sh>
2026-05-31 22:51:15 +0800

    fix: Use eslint-plugin-react-x and eslint-plugin-react-dom instead of eslint-plugin-react

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import tsConfig from "@joshuaavalon/eslint-config-typescript";
-import reactPlugin from "eslint-plugin-react";
+import reactDomPlugin from "eslint-plugin-react-dom";
+// import reactPlugin from "eslint-plugin-react";
 import reactHooksPlugin from "eslint-plugin-react-hooks";
+import reactXPlugin from "eslint-plugin-react-x";
 import { defineConfig } from "eslint/config";
 import perfectionistConfig from "./perfectionist.js";
 import stylisticConfig from "./stylistic.js";
@@ -10,13 +12,22 @@ const config = defineConfig({
     tsConfig,
     stylisticConfig,
     perfectionistConfig,
+    // https://github.com/jsx-eslint/eslint-plugin-react/issues/3977
+    // {
+    //   ...reactPlugin.configs.flat.recommended,
+    //   name: "eslint-plugin-react/recommended"
+    // },
+    // {
+    //   ...reactPlugin.configs.flat["jsx-runtime"],
+    //   name: "eslint-plugin-react/jsx-runtime"
+    // },
     {
-      ...reactPlugin.configs.flat.recommended,
-      name: "eslint-plugin-react/recommended"
+      ...reactXPlugin.configs.recommended,
+      name: "eslint-plugin-react-x"
     },
     {
-      ...reactPlugin.configs.flat["jsx-runtime"],
-      name: "eslint-plugin-react/jsx-runtime"
+      ...reactDomPlugin.configs.recommended,
+      name: "eslint-plugin-react-dom"
     },
     {
       ...reactHooksPlugin.configs.flat.recommended,
