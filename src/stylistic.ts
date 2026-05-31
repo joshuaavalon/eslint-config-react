77fe88977fc2d1eb1ab27547990fab33d02683af
Joshua Avalon <git@avalon.sh>
2026-05-31 23:06:59 +0800

    fix: Remove deprecated rules

--- src/stylistic.ts
+++ src/stylistic.ts
@@ -3,8 +3,14 @@ import { defineConfig } from "eslint/config";
 import type { PrefixRules } from "@joshuaavalon/eslint-config-javascript/utils";
 import type { UnprefixedRuleOptions } from "@stylistic/eslint-plugin";
 
+type DeprecatedRules =
+  | "@stylistic/jsx-indent"
+  | "@stylistic/jsx-props-no-multi-spaces"
+  | "@stylistic/jsx-sort-props";
 
-const rules: Pick<PrefixRules<UnprefixedRuleOptions, "@stylistic/">, `@stylistic/jsx-${string}` & keyof PrefixRules<UnprefixedRuleOptions, "@stylistic/">> = {
+type Rules = Pick<PrefixRules<UnprefixedRuleOptions, "@stylistic/">, `@stylistic/jsx-${string}` & keyof PrefixRules<UnprefixedRuleOptions, "@stylistic/">>;
+
+const rules: Omit<Rules, DeprecatedRules> = {
   "@stylistic/jsx-child-element-spacing": ["error"],
   "@stylistic/jsx-closing-bracket-location": ["error"],
   "@stylistic/jsx-closing-tag-location": ["error"],
@@ -14,16 +20,13 @@ const rules: Pick<PrefixRules<UnprefixedRuleOptions, "@stylistic/">, `@stylistic
   "@stylistic/jsx-equals-spacing": ["error", "never"],
   "@stylistic/jsx-first-prop-new-line": ["error", "multiline-multiprop"],
   "@stylistic/jsx-function-call-newline": ["error", "multiline"],
-  "@stylistic/jsx-indent": ["error", 2],
   "@stylistic/jsx-indent-props": ["error", 2],
   "@stylistic/jsx-max-props-per-line": ["error", { maximum: 1, when: "multiline" }],
   "@stylistic/jsx-newline": ["error", { allowMultilines: false, prevent: true }],
   "@stylistic/jsx-one-expression-per-line": ["error", { allow: "single-child" }],
   "@stylistic/jsx-pascal-case": ["error"],
-  "@stylistic/jsx-props-no-multi-spaces": ["error"],
   "@stylistic/jsx-quotes": ["error", "prefer-double"],
   "@stylistic/jsx-self-closing-comp": ["error", { component: true, html: true }],
-  "@stylistic/jsx-sort-props": ["off"],
   "@stylistic/jsx-tag-spacing": [
     "error",
     {
